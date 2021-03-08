c1907cccd47f8f9ac8271058891cf9692878f65b
error-sudo <66356720+error-sudo@users.noreply.github.com>
2021-03-08 21:37:39 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,8 +5,6 @@ echo "[i]》this application [google cloud console] not eating your storage beca
 sleep 2.5
 echo "[i]》this script made by CyberDark please dont sell this script"
 sleep 0.9
-echo "[I] SCRIPT INI HANYA BISA DI JALANKAN SEKALI SAJA!!!"
- sudo su
  apt-get update -y && apt-get upgrade -y && apt-get full-upgrade -y && apt install git python2 && apt install python2 python bash && apt install mc nano && apt install neofetch
 echo "[i] Installing just begining please stay awake your device.. Dont close this app!!"
  git clone http://github.com/EH30/pureddos
